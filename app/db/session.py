abc5e046f8296ca393a178c753cc09e467c6a4bb
Aryan Shourya <aryanshourya98@gmail.com>
2026-05-18 13:53:40 +0530

    added session and db

---
+++ app/db/session.py
@@ -0,0 +1,33 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from app.config import settings
+
+# The engine
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    settings.DEBUG
+)
+
+# session factory
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_comit=False,
+)
+
+# Base class
+class Base(DeclarativeBase):
+    pass 
+
+#Dependency - routes call this to get DB session
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise 
+
+
+
